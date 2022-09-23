dec8c4ccdb50aeebc0c5181b56bd3ae2c2af4ee6
a <az>
2022-09-23 16:03:09 +0800

    的

--- mock/index.js
+++ mock/index.js
@@ -4,7 +4,14 @@ let app = express();
 app.get('/',function (req,res) {
     res.send(`<h1>Hello, World</h1>`)
 })
-
+app.use('/api/test/project',function(req,res){
+    res.json(Mock.mock({
+        'code':200,
+        'data|1-9':[{
+            'id|+1':1
+        }]
+    }))
+});
 app.listen(8081,function(){
     console.log('服务器已启动')
 })
